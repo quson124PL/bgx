931011af1024f095d87b881371af3c262f26fd90
quson124PL <quson@outlook.com>
2026-04-14 18:53:58 +0200

    Add License to bgx.c

--- bgx.c
+++ bgx.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2026 quson
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
